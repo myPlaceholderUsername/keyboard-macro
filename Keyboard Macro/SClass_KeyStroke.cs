64d9de5e41669e47112ef175782d32fbf4c6529e
whereAmI0 <eric0work0email@gmail.com>
2024-04-11 00:13:54 +0700

    Add supported key check

--- Keyboard Macro/SClass_KeyStroke.cs
+++ Keyboard Macro/SClass_KeyStroke.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
 using WindowsInput;
 using WindowsInput.Native;
 
@@ -9,13 +11,13 @@ namespace Keyboard_Macro
         static InputSimulator inputSimulator = new InputSimulator();
         public static void PressKey(string inStringKey)
         {
-            VirtualKeyCode vkCode = GetVKCodeFromStringKey(inStringKey);
+            VirtualKeyCode vkCode = GetVkCodeFromStringKey(inStringKey);
             inputSimulator.Keyboard.KeyPress(vkCode);
         }
 
         public static void HoldKey(string inStringKey, int inDurationInMiliSec)
         {
-            VirtualKeyCode vkCode = GetVKCodeFromStringKey(inStringKey);
+            VirtualKeyCode vkCode = GetVkCodeFromStringKey(inStringKey);
             inputSimulator.Keyboard.KeyDown(vkCode);
             inputSimulator.Keyboard.Sleep(inDurationInMiliSec);
             inputSimulator.Keyboard.KeyUp(vkCode);
@@ -38,11 +40,84 @@ namespace Keyboard_Macro
             return GetDurationInMiliSec(decDuration);
         }
 
-        // This function uses data type from InputSimulator package
-        static VirtualKeyCode GetVKCodeFromStringKey(string inStrKey)
+        public static bool isVkSupported(string inStrKey)
         {
-            string str_Vk = "VK_" + inStrKey;
-            return (VirtualKeyCode)Enum.Parse(typeof(VirtualKeyCode), str_Vk);
+            try
+            {
+                GetVkCodeFromStringKey(inStrKey);
+                return true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
         }
+
+        // Turns the string in the DGV to VK Code
+        static VirtualKeyCode GetVkCodeFromStringKey(string inStrKey)
+        {
+            VirtualKeyCode vkCode;
+
+            if (isNonNumPadDigit(inStrKey))
+            {
+                vkCode = (VirtualKeyCode)Enum.Parse(typeof(VirtualKeyCode), inStrKey);
+                return vkCode;
+            }
+
+            // Try just adding VK_ to beginning of string key
+            try
+            {
+                string str_Vk = "VK_" + inStrKey.ToUpper();
+                vkCode = (VirtualKeyCode)Enum.Parse(typeof(VirtualKeyCode), str_Vk);
+            }
+            // Find in dictionary if failed
+            catch
+            {
+                vkCode = dict_SupportedKeys[inStrKey];
+            }
+
+            return vkCode;
+        }
+
+        static bool isNonNumPadDigit(string inStrKey)
+        {
+            if (inStrKey.Length > 1)
+                return false;
+
+            return Char.IsDigit(inStrKey[0]);
+        }
+
+        private static Dictionary<string, VirtualKeyCode> dict_SupportedKeys = new Dictionary<string, VirtualKeyCode>()
+        {
+            // NumPad number
+            { Keys.NumPad0.ToString(), VirtualKeyCode.NUMPAD0 },
+            { Keys.NumPad1.ToString(), VirtualKeyCode.NUMPAD1 },
+            { Keys.NumPad2.ToString(), VirtualKeyCode.NUMPAD2 },
+            { Keys.NumPad3.ToString(), VirtualKeyCode.NUMPAD3 },
+            { Keys.NumPad4.ToString(), VirtualKeyCode.NUMPAD4 },
+            { Keys.NumPad5.ToString(), VirtualKeyCode.NUMPAD5 },
+            { Keys.NumPad6.ToString(), VirtualKeyCode.NUMPAD6 },
+            { Keys.NumPad7.ToString(), VirtualKeyCode.NUMPAD7 },
+            { Keys.NumPad8.ToString(), VirtualKeyCode.NUMPAD8 },
+            { Keys.NumPad9.ToString(), VirtualKeyCode.NUMPAD9 },
+
+            // Arrow Keys
+            { Keys.Left.ToString(), VirtualKeyCode.LEFT },
+            { Keys.Right.ToString(), VirtualKeyCode.RIGHT },
+            { Keys.Up.ToString(), VirtualKeyCode.UP },
+            { Keys.Down.ToString(), VirtualKeyCode.DOWN },
+
+            { Keys.Space.ToString(), VirtualKeyCode.SPACE },
+            { Keys.Enter.ToString(), VirtualKeyCode.RETURN },
+
+            // Math
+            { Keys.Oemplus.ToString(), VirtualKeyCode.OEM_PLUS },
+            { Keys.OemMinus.ToString(), VirtualKeyCode.OEM_MINUS },
+
+            // Punctuation
+            { Keys.Oemcomma.ToString(), VirtualKeyCode.OEM_COMMA },
+            { Keys.OemPeriod.ToString(), VirtualKeyCode.OEM_PERIOD },
+        };
     }
 }
